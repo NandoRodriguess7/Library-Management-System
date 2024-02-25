d54d906eb85e38e4790ca1b50e72b3800c8803db
Fernando Rodrigues Alves <fernando.rodrigues.alves14@gmail.com>
2024-02-25 01:20:46 -0300

    Implementing the Admin and DataBase classes, creating the NormalUser class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,42 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
-
+	
+    static Scanner s;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Welcome to Library Management System!\n"
+				+ "1. Login\n2. New User");
+		
+		s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		
+		switch(n) {
+		case 1: login();
+		case 2: newUser();
+		default: System.out.println("Error!");
+		}
+		
+	}
+	
+	private static void login() {
+		System.out.println("Enter phone number: ");
+		String phoneNumber = s.next();
+		System.out.println("Enter email: ");
+		String email = s.next();
+	}
+	
+	private static void newUser() {
+		System.out.println("Enter name: ");
+		String name = s.next();
+		System.out.println("Enter phone number: ");
+		String phoneNumber = s.next();
+		System.out.println("Enter email: ");
+		String email = s.next();
 	}
 
 }
